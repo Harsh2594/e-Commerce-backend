39032e4cce8d9cb771b680503a1d67169f88b811
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-09 14:51:30 +0530

    Add type, requiredproperty to schema

--- auth-service/src/models/product.model.js
+++ auth-service/src/models/product.model.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 //product schema
 const productSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  description: String,
+  productName: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
