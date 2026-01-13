0b66b4654fd2627b2c9cabd59ac49dae72f836de
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-13 16:01:49 +0530

    Add user routes

---
+++ auth-service/src/routes/user.routes.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const verifyToken = require("../middlewares/verifyToken");
+const userController = require("../controllers/user.controller");
+
+//change_password
+/**
+ * @swagger
+ * /api/users/changePassword:
+ *   put:
+ *     summary: Change user password
+ *     tags: [Users Profile]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - oldPassword
+ *               - newPassword
+ *             properties:
+ *               oldPassword:
+ *                 type: string
+ *                 example: oldPass@123
+ *               newPassword:
+ *                 type: string
+ *                 example: newPass@456
+ *     responses:
+ *       200:
+ *         description: Password changed successfully
+ *       400:
+ *         description: Validation error
+ *       401:
+ *         description: Invalid old password
+ */
+
+router.put("/changePassword", verifyToken, userController.changePassword);
+
+module.exports = router;
