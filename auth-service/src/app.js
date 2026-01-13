cb1c2b38b880c46ce1937f1fb04f3d0aa38c466c
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-13 12:01:36 +0530

    Rename user,users in route call

--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth.routes"));
 
-app.use("/api/users", require("./routes/user.routes"));
+app.use("/api/products", require("./routes/product.routes"));
 
 const swaggerUI = require("swagger-ui-express");
 const swaggerSpec = require("./config/swagger");
