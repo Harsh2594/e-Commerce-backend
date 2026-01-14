c5fca78f4c1b84fe23e50b72b8c85cfe5949811a
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-14 13:30:15 +0530

    Add route mounting

--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -10,6 +10,8 @@ app.use("/api/auth", require("./routes/auth.routes"));
 
 app.use("/api/products", require("./routes/product.routes"));
 
+app.use("/api/users", require("./routes/user.routes"));
+
 const swaggerUI = require("swagger-ui-express");
 const swaggerSpec = require("./config/swagger");
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
