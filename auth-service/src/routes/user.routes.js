6cf26e3a64d79f4bd056beac74a1ef242b2bffa2
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-14 14:39:23 +0530

    Add get user_profile method

--- auth-service/src/routes/user.routes.js
+++ auth-service/src/routes/user.routes.js
@@ -39,4 +39,37 @@ const userController = require("../controllers/user.controller");
 
 router.put("/changePassword", verifyToken, userController.changePassword);
 
+//get_user_profile
+/**
+ * @swagger
+ * /api/users/User-Profile:
+ *   get:
+ *     summary: Get User detail
+ *     tags: [Users Profile]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User profile fetched successfully changed
+ *         content:
+ *           application/json:
+ *             schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               role:
+ *                 type: string
+ *       401:
+ *         description: Unauthorized or invalid token
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Internal server error
+ */
+router.get("/User-Profile", verifyToken, userController.getProfile);
 module.exports = router;
