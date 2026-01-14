fa4d4bce39816446f1b8551dade31e4086ba8c24
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-14 13:38:11 +0530

    Add router for forgotPassword and resetpassword

--- auth-service/src/routes/auth.routes.js
+++ auth-service/src/routes/auth.routes.js
@@ -8,6 +8,8 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
+const validateEmail = require("../middlewares/validateEmail");
+const verifyToken = require("../middlewares/verifyToken");
 
 //signup
 /**
@@ -77,5 +79,64 @@ router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 //logout
 router.post("/logout", authController.logout);
+//Forgot_Password
+/**
+ * @swagger
+ * /api/auth/forgotPassword:
+ *   post:
+ *     summary: Forgot Password
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: user1@gmail.com
+ *     responses:
+ *       200:
+ *         description: Link has been sent to your email
+ *       400:
+ *         description: Email is required
+ *       404:
+ *         description: User not found
+ *
+ */
+router.post("/forgotPassword", validateEmail, authController.forgotPassword);
+//Reset_password
+/**
+ * @swagger
+ * /api/auth/resetPassword:
+ *   post:
+ *     summary: Reset Password
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - token
+ *               - newPassword
+ *             properties:
+ *               token:
+ *                 type: string
+ *                 example: reset_token_here
+ *               newPassword:
+ *                 type: string
+ *                 example: user1@123
+ *     responses:
+ *       200:
+ *         description: Password reset successfully
+ *       400:
+ *         description: Invalid or expired token
+ */
+router.post("/resetPassword", authController.resetPassword);
 
 module.exports = router;
