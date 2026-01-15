25dd3db8e76ee1427dee06c3c78e84afac9f9863
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-15 16:42:00 +0530

    Add product image property in product schema

--- auth-service/src/models/product.model.js
+++ auth-service/src/models/product.model.js
@@ -5,6 +5,7 @@ const productSchema = new mongoose.Schema({
   productName: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String, required: true },
+  productImage: { type: String, required: false },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
