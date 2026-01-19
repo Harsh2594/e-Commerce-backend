12c4694145e7d69b6b7db73a3a305cdbdce710bd
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-19 16:50:11 +0530

    Add category property in schema

--- auth-service/src/models/product.model.js
+++ auth-service/src/models/product.model.js
@@ -5,7 +5,8 @@ const productSchema = new mongoose.Schema({
   productName: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String, required: true },
-  productImage: { type: String, required: false },
+  productImage: [{ type: String, required: false }],
+  category: { type: String, required: true },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
