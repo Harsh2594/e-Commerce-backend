d87d88ed7910fd7c7c9764ed9ce5efd41fa9b8bd
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-20 11:33:10 +0530

    Implement Product pagination

--- auth-service/src/controllers/product.controller.js
+++ auth-service/src/controllers/product.controller.js
@@ -92,12 +92,19 @@ exports.updateProduct = async (req, res) => {
 
 exports.getProducts = async (req, res) => {
   try {
-    const products = await Product.find();
-    res.status(200).json({
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
+
+    const totalProducts = await Product.countDocuments();
+    const products = await Product.find().skip(skip).limit(limit);
+    return res.status(200).json({
       success: true,
-      message: "Products find successfully",
-      data: products,
-      error: null,
+      message: "Product fetched successfully",
+      totalProducts,
+      currentPage: page,
+      totalPages: Math.ceil(totalProducts / limit),
+      products,
     });
   } catch (err) {
     res.status(500).json({
