874aecafbdcf61c16b5e1b1a0502b0e57c9c0ea1
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-20 15:06:40 +0530

    Add category schema

---
+++ auth-service/src/models/category.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+//Category_Schema
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    slug: {
+      type: String,
+      lowecase: true,
+      unique: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Category", categorySchema);
