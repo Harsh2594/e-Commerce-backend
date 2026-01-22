b88df25a2a230bae2e80951bf6a7a8412a4fd867
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-22 10:30:01 +0530

    Add cart schema

---
+++ auth-service/src/models/cart.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+//cart_item_schema
+const cartItem = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    default: 1,
+    min: 1,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+//Cart_Schema
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    items: [cartItem],
+    orderTotal: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Cart", cartSchema);
