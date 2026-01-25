d905291048631da33c39e02fa44b06fdc58530fa
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-25 16:20:53 +0530

    create order schema

---
+++ auth-service/src/models/order.model.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const orderItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [orderItemSchema],
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    orderStatus: {
+      type: String,
+      enum: ["pending", "confirmed", "shipped", "delivered", "cancelled"],
+      default: "pending",
+    },
+    paymentMethod: {
+      type: String,
+      enum: ["COD", "CARD", "UPI"],
+      required: true,
+    },
+    shippingAddress: {
+      type: Object,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Order", orderSchema);
