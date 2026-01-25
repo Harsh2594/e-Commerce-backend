8f60e58c0ebf73643cc0dc8a2f19d2c0aed12ae4
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-25 16:35:42 +0530

    add adminOrder controller

---
+++ auth-service/src/controllers/adminOrder.controller.js
@@ -0,0 +1,63 @@
+const Product = require("../models/product.model");
+const User = require("../models/user.model");
+const Order = require("../models/order.model");
+const Cart = require("../models/cart.model");
+
+//getAllorders
+exports.getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find()
+      .populate("user", "name email")
+      .populate("items.product", "name price");
+    return res.status(200).json({
+      success: true,
+      message: orders.length ? "orders found" : "No orders found",
+      data: orders,
+      error: null,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "server error",
+      data: null,
+      error: err.message,
+    });
+  }
+};
+
+//change order status
+exports.changeOrderStatus = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const { orderStatus } = req.body;
+
+    //find order
+    const order = await Order.findById(orderId)
+      .populate("user", "name email")
+      .populate("items.product", "productName price");
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order not found",
+        data: [],
+        error: null,
+      });
+    }
+    order.orderStatus = orderStatus;
+    await order.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Order status updated",
+      data: order,
+      error: null,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "signup failed",
+      data: null,
+      error: err.message,
+    });
+  }
+};
