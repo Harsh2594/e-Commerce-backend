48115c5bb1603d913ee24d0ff37d55907294ba09
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-26 17:32:01 +0530

    Make payment Schema

---
+++ auth-service/src/models/payment.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+//payment Schema
+const paymentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    order: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["success", "failed", "pending"],
+      default: "pending",
+    },
+    paymentMethod: {
+      type: String,
+      default: "Mock",
+    },
+    transactionId: {
+      type: String,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Payment", paymentSchema);
