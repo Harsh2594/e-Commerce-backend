1d5964fdeddc230915b60fda5886e96f994b7e59
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-26 14:54:36 +0530

    fix YAMLSemanticError

--- auth-service/src/routes/user.routes.js
+++ auth-service/src/routes/user.routes.js
@@ -105,7 +105,7 @@ router.get("/User-Profile", verifyToken, userController.getProfile);
  *                 example: harsh
  *               phoneNumber:
  *                 type: string
- *                 example:9800000000
+ *                 example: 9898989898
  *               address:
  *                 type: string
  *                 example: Kanpur Nagar
