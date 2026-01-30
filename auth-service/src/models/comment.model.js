d3147f7eea6bd759d065fe0b965a13e7d76eccca
Harsh2594 <harsh.sgm13@gmail.com>
2026-01-30 18:03:48 +0530

    Add comment Schema

---
+++ auth-service/src/models/comment.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 500,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+commentSchema.index({ product: 1 });
+commentSchema.index({ user: 1 });
+
+module.exports = mongoose.model("Comment", commentSchema);
