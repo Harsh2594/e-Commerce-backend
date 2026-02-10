83381d5e19f930d2b613091ff0c52692aa3df626
Harsh2594 <harsh.sgm13@gmail.com>
2026-02-10 15:53:52 +0530

    add rewardPoint as a field

--- auth-service/src/models/user.model.js
+++ auth-service/src/models/user.model.js
@@ -45,6 +45,10 @@ const userSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
+  rewardPoints: {
+    type: Number,
+    default: 0,
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
