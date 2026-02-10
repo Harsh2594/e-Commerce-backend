d69aedab3c64f106867b96bb7d8ff2277ba131b1
Harsh2594 <harsh.sgm13@gmail.com>
2026-02-10 15:52:42 +0530

    Add PostTransaction Schema

---
+++ auth-service/src/models/pointTransaction.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const pointTransactionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    points: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["EARN", "REDEEM"],
+    },
+    source: {
+      type: String,
+      enum: ["POST_SALE", "ORDER_REDEEM"],
+    },
+    referenceId: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("PointTransaction", pointTransactionSchema);
