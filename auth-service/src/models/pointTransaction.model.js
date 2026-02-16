a47958ee25491cf16b731af036a5836ecbc6c56f
Harsh2594 <harsh.sgm13@gmail.com>
2026-02-16 10:00:16 +0530

    Add some enum value to type and source field

--- auth-service/src/models/pointTransaction.model.js
+++ auth-service/src/models/pointTransaction.model.js
@@ -13,11 +13,11 @@ const pointTransactionSchema = new mongoose.Schema(
     },
     type: {
       type: String,
-      enum: ["EARN", "REDEEM"],
+      enum: ["EARN", "REDEEM", "REDEEM_REFUND"],
     },
     source: {
       type: String,
-      enum: ["POST_SALE", "ORDER_REDEEM"],
+      enum: ["POST_SALE", "ORDER_REDEEM", "ORDER_CANCEL"],
     },
     referenceId: {
       type: mongoose.Schema.Types.ObjectId,
