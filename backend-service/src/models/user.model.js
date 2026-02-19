76a86603ea29865d1c7a7cd5eab4bcb300bd3159
Harsh2594 <harsh.sgm13@gmail.com>
2026-02-19 13:10:14 +0530

    Add publicId field and preHook

--- backend-service/src/models/user.model.js
+++ backend-service/src/models/user.model.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
+const generateUserId = require("../utils/generateCustomId");
 
 const userSchema = new mongoose.Schema({
+  publicId: {
+    type: String,
+    unique: true,
+  },
   name: {
     type: String,
     required: true,
@@ -49,6 +54,14 @@ const userSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
+});
+
+//use_pre_save_hook
+userSchema.pre("save", async function () {
+  if (!this.publicId) {
+    const username = this.name.toLowerCase().replace(/[^a-z0-9]/g, "");
+    this.publicId = await generateUserId(`user_${username}`, username);
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
