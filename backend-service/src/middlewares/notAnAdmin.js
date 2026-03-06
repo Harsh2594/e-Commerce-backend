7971b6d22f2ef7be9366c55e747de297ee2fb235
Harsh2594 <harsh.sgm13@gmail.com>
2026-03-06 16:52:18 +0530

    add req.user in validation and fix response structure

--- backend-service/src/middlewares/notAnAdmin.js
+++ backend-service/src/middlewares/notAnAdmin.js
@@ -1,6 +1,11 @@
 module.exports = (req, res, next) => {
-  if (req.user.role !== "user") {
-    return res.status(403).json({ message: "User access only" });
+  if (!req.user || req.user.role !== "user") {
+    return res.status(403).json({
+      success: false,
+      message: "User access only",
+      data: null,
+      error: null,
+    });
   }
   next();
 };
