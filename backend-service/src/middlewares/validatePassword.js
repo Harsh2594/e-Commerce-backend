fe1135965044999117a67986c0bbfd6349101b9b
Harsh2594 <harsh.sgm13@gmail.com>
2026-03-06 17:08:16 +0530

    fix success: true Returned on First Error

--- backend-service/src/middlewares/validatePassword.js
+++ backend-service/src/middlewares/validatePassword.js
@@ -2,15 +2,17 @@ module.exports = (req, res, next) => {
   const { password } = req.body;
   if (!password) {
     return res.status(400).json({
-      success: true,
+      success: false,
       message: "Password is required",
+      data: null,
+      error: null,
     });
   }
   const passwordregex =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   if (!passwordregex.test(password)) {
     return res.status(400).json({
-      success: true,
+      success: false,
       message:
         "password must be at least 8 characters and include uppercase, lowercase,number,and special character",
       data: null,
