1910c94b4f8d87567017ac34c05cb1585d7adaa3
Harsh2594 <harsh.sgm13@gmail.com>
2026-03-06 17:05:14 +0530

    fix ReferenceError: allowedExt is not defined

--- backend-service/src/middlewares/upload.js
+++ backend-service/src/middlewares/upload.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
 const path = require("path");
-
+const crypto = require("crypto");
 // absolute path to uploads folder
 const uploadDir = path.join(__dirname, "..", "..", "uploads");
 
@@ -10,7 +10,11 @@ const storage = multer.diskStorage({
     cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    const uniqueName = Date.now() + path.extname(file.originalname);
+    const uniqueName =
+      Date.now() +
+      "-" +
+      crypto.randomBytes(8).toString("hex") +
+      path.extname(file.originalname);
     cb(null, uniqueName);
   },
 });
@@ -18,9 +22,11 @@ const storage = multer.diskStorage({
 //file filter
 const fileFilter = (req, file, cb) => {
   const allowedType = /jpeg|png|jpg/;
+  const allowedExt = /\.(jpg|jpeg|png)$/i;
+  const isExtValid = allowedExt.test(path.extname(file.originalname));
   const isValid = allowedType.test(file.mimetype);
 
-  if (isValid) {
+  if (isValid && isExtValid) {
     cb(null, true);
   } else {
     cb(new Error("Only images are allowed"), false);
