6b57204655600f213bfb9d05c5888ea1015d36a7
Harsh2594 <harsh.sgm13@gmail.com>
2026-03-06 17:58:03 +0530

    Change title and description

--- backend-service/src/config/swagger.js
+++ backend-service/src/config/swagger.js
@@ -4,9 +4,9 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Auth Service API",
+      title: "E-commerce Service API",
       version: "1.0.0",
-      description: "Swagger documentation for Auth & User APIs",
+      description: "Swagger documentation for E-commerce APIs",
     },
     servers: [
       {
