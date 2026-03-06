a186b3dcd4a049c34a2dd6fc6d62c14861893c2a
Harsh2594 <harsh.sgm13@gmail.com>
2026-03-06 17:32:46 +0530

    Add authHeader validation

--- backend-service/src/middlewares/verifyToken.js
+++ backend-service/src/middlewares/verifyToken.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const env = require("../config/env");
 
 module.exports = (req, res, next) => {
   try {
@@ -6,20 +7,33 @@ module.exports = (req, res, next) => {
 
     if (!authHeader) {
       return res.status(401).json({
+        success: false,
         message: "Token missing",
+        data: null,
+        error: null,
       });
     }
-
+    if (!authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid authorization format",
+        data: null,
+        error: null,
+      });
+    }
     const token = authHeader.split(" ")[1];
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, env.jwtSecret);
 
     req.user = decoded;
 
     next();
   } catch (err) {
     return res.status(401).json({
+      success: false,
       message: "Invalid token",
+      data: null,
+      error: err.message,
     });
   }
 };
